caa2fa73f98604de57b72bcbf543b006ce8d418a
James Njenga <knjenga94@gmail.com>
2022-10-29 09:31:01 +0300

    Added CRUD operations on SQL

---
+++ delete.sql
@@ -0,0 +1,2 @@
+--DELETE FROM [table name] WHERE [column name]=[value]
+DELETE FROM cats WHERE id=3
---
+++ select.sql
@@ -0,0 +1,5 @@
+--SELECT * FROM cats
+
+-- SELECT * FROM cats WHERE name="Maru"
+
+SELECT DISTINCT name FROM cats
